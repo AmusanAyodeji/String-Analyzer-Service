c0763fd472cbca26028ec0b648b46f3f53d2d27c
Ayodeji Amusan <ayobayonleamusan1@gmail.com>
2025-10-23 00:48:57 +0100

    finished project and made ngrok deployment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from mangum import Mangum
 import strings
+import uvicorn
+from pyngrok import ngrok
+import threading
 
 app = FastAPI()
 
@@ -10,4 +12,14 @@ app.include_router(strings.router)
 def home():
     return {"details":"HNG Stage 1 Task"}
 
-handler = Mangum(app)
+def start_ngrok():
+    # Open a tunnel on the same port as uvicorn
+    public_url = ngrok.connect(8000)
+    print(f"\n🚀 Public URL: {public_url}\n")
+
+def start_uvicorn():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    threading.Thread(target=start_ngrok).start()
+    start_uvicorn()
